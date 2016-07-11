986117e57b1ffaf745c44c9cca4a11b566d7395b
TeodorNikolov <teodor.stefanov.nikolov@gmail.com>
2016-07-12 00:53:40 +0300

    Add files via upload

---
+++ Path of the Warrior/Items/Consumable.cs
@@ -0,0 +1,37 @@
+﻿using Items;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace  PathOfTheWarrior.Items
+{
+    public abstract class Consumable : Item
+    {
+
+        private int bonusHealth;
+        public int BonusHealth { 
+            get 
+        { 
+            return bonusHealth; 
+        }
+            set 
+        { 
+            this.bonusHealth = value; 
+        } 
+        }
+        public virtual int DrinkPotion(ICharacter character)
+        {
+            if (character.HP > BonusHealth)
+            {
+               return character.HP += BonusHealth;
+            }
+            else
+            {
+                return character.HP;
+            }
+        }
+
+    }
+}
