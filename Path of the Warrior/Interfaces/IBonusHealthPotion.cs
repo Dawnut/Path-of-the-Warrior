023376678aa39e6bf0f05bf0cbce47e3ba31e0cd
TeodorNikolov <teodor.stefanov.nikolov@gmail.com>
2016-07-11 23:56:34 +0300

    Add files via upload

---
+++ Path of the Warrior/Interfaces/IBonusHealthPotion.cs
@@ -0,0 +1,10 @@
+﻿namespace PathOfTheWarrior.Interfaces
+{
+    public interface IBonusHealthPotion
+
+    {
+        int UpperHealth { get; set; }
+        //increase maximum health
+        void PermaAddHealth(ICharacter character);
+    }
+}
