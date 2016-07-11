a53d2f5a15222497478fbf31c446f0037f7ca5c9
TeodorNikolov <teodor.stefanov.nikolov@gmail.com>
2016-07-12 01:47:13 +0300

    Add files via upload
    Added class DMGPotion.

---
+++ Path of the Warrior/Items/DMGPotion.cs
@@ -0,0 +1,24 @@
+﻿using PathOfTheWarrior.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace PathOfTheWarrior.Items
+{
+    class DMGPotion : Consumable, IHasAvatar
+    {
+        public ImageSource Avatar { get; set; }
+
+        public HealthPotion(int bonusStat)
+            : base(bonusStat) { }
+
+        public override void DrinkPotion(ICharacter character)
+        {
+            character.DMG += this.BonusStat;
+        }
+
+    }
+}
