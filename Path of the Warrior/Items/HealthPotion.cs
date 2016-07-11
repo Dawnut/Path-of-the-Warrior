957a7c2aa8e2b2ab03c89187b25fc2a9f3668885
TeodorNikolov <teodor.stefanov.nikolov@gmail.com>
2016-07-12 01:27:54 +0300

    Add files via upload

---
+++ Path of the Warrior/Items/HealthPotion.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PathOfTheWarrior.Items
+{
+    class HealthPotion : Consumable
+    {
+        private int bonusHealth;
+        public int BonusHealth { 
+            get 
+            {
+                return bonusHealth = BonusStat; 
+            } 
+            set 
+            { 
+                this.bonusHealth = value;
+            } 
+        }
+        public abstract override int DrinkPotion(ICharacter character)
+        {
+            return character.HP += bonusHealth;
+        }
+
+    }
+}
