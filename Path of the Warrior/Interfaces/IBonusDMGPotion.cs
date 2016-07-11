70dc35f6cb04384ff207fb627a36025d7a94fdb1
TeodorNikolov <teodor.stefanov.nikolov@gmail.com>
2016-07-12 00:00:05 +0300

    Add files via upload

---
+++ Path of the Warrior/Interfaces/IBonusDMGPotion.cs
@@ -0,0 +1,10 @@
+﻿namespace PathOfTheWarrior.Interfaces
+{
+    public interface IBonusDMGPotion
+
+    {
+        int UpperDMG { get; set; }
+        //increase maximum DMG
+        void PermaAddDMG(ICharacter character);
+    }
+}
