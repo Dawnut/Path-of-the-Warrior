d6861d4dc56a021c7614f6e84eef8bac03da74cb
TeodorNikolov <teodor.stefanov.nikolov@gmail.com>
2016-07-12 00:05:09 +0300

    Add files via upload

---
+++ Path of the Warrior/Interfaces/IFreeWinTiccket.cs
@@ -0,0 +1,10 @@
+﻿namespace PathOfTheWarrior.Interfaces
+{
+    public interface IFreeWinTiccket
+
+    {
+        int OneFreeWin { get; set; }
+        //increase maximum DMG
+        void PermaAddDMG(ICharacter character);
+    }
+}
